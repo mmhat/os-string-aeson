8945b0d9df814910fc5aed3b9ce1b1487a81ae54
Mann mit Hut <github+mmhat@with-h.at>
2024-08-12 04:20:33 +0200

    Require filepath even though we have os-string >2

--- stack/test-stack.sh
+++ stack/test-stack.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
 for version in '9.2' '9.4' '9.6'; do
     for flag in '+os-string' ''; do
         stack_yaml="stack/stack.ghc-${version}${flag}.yaml"
         echo "### TESTING ${stack_yaml}"
         rm -rf .stack-work stack.yaml stack.yaml.lock
-        ln -s "${stack_yaml}" stack.yaml
-        ln -s "${stack_yaml}.lock" stack.yaml.lock
+        cp "${stack_yaml}" stack.yaml
         stack test --pedantic
+        cp stack.yaml.lock "${stack_yaml}.lock"
     done
 done
+
+rm -rf .stack-work stack.yaml stack.yaml.lock
+ln -s "${stack_yaml}" stack.yaml
+ln -s "${stack_yaml}.lock" stack.yaml.lock
